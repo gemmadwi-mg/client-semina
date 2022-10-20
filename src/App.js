d7e42c56d676d7f3bbe728372ffc8f290047d48c
GDwiP <gemma.19051@mhs.unesa.ac.id.com>
2022-10-20 12:53:10 +0700

    menampilkan data dengan array

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import Button from './components/Button';
 import './App.css';
+import { Hello } from './Hello';
+import { Title } from './Title';
 
 function App() {
+  const users = ['gemma', 'nungky', 'Hiji'];
+
   return (
     <>
       <h1>Hello React</h1>
+      <h1><Hello /></h1>
+      <Title name="Gemma Dwi Prasetya" />
+      <br />
+      <Title name="Dwi" />
+      <br />
+      <Title />
+      <Button onClick={() => alert('Click save')}>Save</Button>
+      <ul>
+        {users.map((user, index) => { return <li key={index}>{user}</li> })}
+      </ul>
     </>
   );
 }
