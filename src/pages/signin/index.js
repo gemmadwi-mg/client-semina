61b9bf77332e513c964c1a445d2f63db9cbc6008
GDwiP <gemma.19051@mhs.unesa.ac.id.com>
2022-10-21 08:34:03 +0700

    axios && error alert

--- src/pages/signin/index.js
+++ src/pages/signin/index.js
@@ -2,9 +2,10 @@ import React, { useState } from 'react'
 import Container from 'react-bootstrap/Container';
 import Card from 'react-bootstrap/Card';
 import Form from 'react-bootstrap/Form';
-import { Button } from 'react-bootstrap';
 import TextInputWithLabel from '../../components/TextInputWithLabel';
 import SButton from '../../components/Button';
+import axios from 'axios';
+import SAlert from '../../components/Alert';
 
 function PageSignIn() {
     const [form, setForm] = useState({
@@ -16,8 +17,23 @@ function PageSignIn() {
         setForm({ ...form, [e.target.name]: e.target.value })
     }
 
+    const handleSubmit = async () => {
+        try {
+            const response = axios.post('http://localhost:4000/api/v1/cms/auth/signin', {
+                email: form.email,
+                password: form.password
+
+            });
+            console.log(response)
+        } catch (error) {
+            console.log(error)
+        }
+
+    }
+
     return (
         <Container md={12}>
+            <SAlert message={'test'}  type='danger'/>
             <Card style={{ width: '50%' }} className='m-auto mt-5'>
                 <Card.Body>
                     <Card.Title className='text-center'>Form Signin</Card.Title>
@@ -26,7 +42,7 @@ function PageSignIn() {
 
                         <TextInputWithLabel label="Password" name='password' value={form.password} type="password" placeholder="Password" onChange={handleChange} />
 
-                        <SButton variant="primary">
+                        <SButton action={handleSubmit} variant="primary">
                             Submit
                         </SButton>
                     </Form>
