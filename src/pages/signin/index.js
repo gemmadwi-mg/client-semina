37c67a1134c615cd9ab246dc7fd10f5494c49deb
GDwiP <gemma.19051@mhs.unesa.ac.id.com>
2022-10-21 09:01:50 +0700

    loading

--- src/pages/signin/index.js
+++ src/pages/signin/index.js
@@ -13,11 +13,20 @@ function PageSignIn() {
         password: ''
     })
 
+    const [alert, setAlert] = useState({
+        status: false,
+        message: '',
+        type: 'danger'
+    })
+
+    const [isLoading, setIsLoading] = useState(false);
+
     const handleChange = (e) => {
         setForm({ ...form, [e.target.name]: e.target.value })
     }
 
     const handleSubmit = async () => {
+        setIsLoading(true);
         try {
             const response = axios.post('http://localhost:4000/api/v1/cms/auth/signin', {
                 email: form.email,
@@ -25,15 +34,26 @@ function PageSignIn() {
 
             });
             console.log(response)
+
+            setIsLoading(false);
         } catch (error) {
-            console.log(error)
+            setIsLoading(false);
+            console.log(error.response.data.msg)
+            setAlert({
+                status: true,
+                type: 'danger',
+                message: error?.response?.data?.msg ?? 'Internal server error',
+            })
+
         }
 
     }
 
     return (
-        <Container md={12}>
-            <SAlert message={'test'}  type='danger'/>
+        <Container md={12} className='my-5'>
+            <div className='m-auto' style={{ width: '50%' }}>
+                {alert.status && <SAlert message={alert.message} type={alert.type} />}
+            </div>
             <Card style={{ width: '50%' }} className='m-auto mt-5'>
                 <Card.Body>
                     <Card.Title className='text-center'>Form Signin</Card.Title>
@@ -42,7 +62,7 @@ function PageSignIn() {
 
                         <TextInputWithLabel label="Password" name='password' value={form.password} type="password" placeholder="Password" onChange={handleChange} />
 
-                        <SButton action={handleSubmit} variant="primary">
+                        <SButton loading={isLoading} disabled={isLoading} action={handleSubmit} variant="primary">
                             Submit
                         </SButton>
                     </Form>
